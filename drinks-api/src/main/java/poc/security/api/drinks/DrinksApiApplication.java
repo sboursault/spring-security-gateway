418febc80a0d870697214041342ce8750d33b9f5
sboursault <sboursault@protonmail.com>
2018-07-31 15:19:59 +0200

    rename whisky api to drinks api

---
+++ drinks-api/src/main/java/poc/security/api/drinks/DrinksApiApplication.java
@@ -0,0 +1,12 @@
+package poc.security.api.drinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksApiApplication.class, args);
+	}
+}
---
+++ drinks-api/src/test/java/poc/security/api/drinks/DrinksApiApplicationTests.java
@@ -0,0 +1,16 @@
+package poc.security.api.drinks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrinksApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
